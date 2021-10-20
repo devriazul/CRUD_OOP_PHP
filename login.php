ae5ae2cd604c7764cf9d45424ce7ed2ca3324e11
devriazul <riazul56@gmail.com>
2021-10-20 21:31:59 +0600

    style added

---
+++ login.php
@@ -0,0 +1,39 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <style type="text/css">
+        #text{
+            height: 25px;
+            border-radius: 5px;
+            padding: 4px;
+            border:solid thin #aaa;
+        }
+
+        #button{
+            padding: 10px;
+            width: 100px;
+            color: white;
+            background-color: lightblue;
+            border: none;
+        }
+        #box{
+            background-color: grey;
+            margin: auto;
+            width: 300px;
+            padding: 20px;
+        }
+
+    </style>
+    
+</body>
+</html>
